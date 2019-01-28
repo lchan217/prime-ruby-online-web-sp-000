8f79b8282c8e4d1c6fdb33eb7ca0fd14761c7c3f
Lizzie Chan <lchan217@gmail.com>
2019-01-28 13:43:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,3 @@
 def prime?(num)
-  range = [2..num]
-  array = range.to_a    
-  if num < 2 
-      false 
-  elsif
-    array.each do |x|
-    x % num != 0
-    true
-  else
-    false
-  end
-end
+  
 end
