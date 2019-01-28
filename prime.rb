560c1d3266177527853c02f5c141434574c364ac
Lizzie Chan <lchan217@gmail.com>
2019-01-28 13:40:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  range = [2..num]
+  array = range.to_a    
+  if num < 2 
+      false 
+  elsif
+    array.each do |x|
+    x % num != 0
+    true
+  else
+    false
+  end
+end
+end
