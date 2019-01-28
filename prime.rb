6c992c654d1bc5e71db66512434f26c4e06a02b4
Lizzie Chan <lchan217@gmail.com>
2019-01-28 13:54:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,16 @@
 def prime?(num)
+  i = 2
   range = [2..num]
   array = range.to_a    
-  if num < 2 
-      false 
-  elsif
-    array.each do |x|
-    x % num != 0
-    true
-  else
-    false
-  end
-end
+    if num < 2 
+        false 
+    elsif
+      while i > 2 && num % i == 0
+        return false
+      i+=1
+      end
+    else 
+      false   
+      
+    end
 end
